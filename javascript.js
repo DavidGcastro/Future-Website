4a554e7d543b462adac4683775e0674d5e0653ec
David <davidgcastro93@gmail.com>
2017-07-28 22:05:08 -0400

    Responsive is done

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
+
+
 $(document).ready(function() {
   var first = $("#image").attr('src', "https://images.unsplash.com/photo-1497979520324-8140b1524b8d?dpr=2&auto=format&fit=crop&w=1500&h=2185&q=80&cs=tinysrgb&crop=");
 
   function getImages(x) {
-    $('#image').attr('src', x).hide().fadeIn(700);
+   $('#image').attr('src', x).hide().fadeIn(500);
   }
   var image1 = "https://images.unsplash.com/photo-1498335746477-0c73d7353a07?dpr=2&auto=format&fit=crop&w=1500&h=2100&q=80&cs=tinysrgb&crop=";
   var image2 = "https://images.unsplash.com/photo-1498842812179-c81beecf902c?dpr=2&auto=format&fit=crop&w=1500&h=2250&q=80&cs=tinysrgb&crop=";
@@ -23,7 +25,15 @@ $(document).ready(function() {
         console.log("done");
         timedLoop();
       }
-    }, 8000)
+    }, 5000)
   }
   timedLoop();
 });
+
+//mobile menu 
+
+
+
+
+
+
