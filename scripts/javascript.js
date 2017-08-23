4c7d12029f9b5f7f5247e9235c463904cc5623f0
David <davidgcastro93@gmail.com>
2017-08-23 00:58:48 -0400

    Added mobile menu. Still need to fix the bug that makes desktop menu dissapear after using mobile trigger. Also need to style mobile trigger to have transition effect.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
   var first = $("#image").attr('src', "https://s-media-cache-ak0.pinimg.com/736x/16/5d/10/165d106ff9844b31ba1887b224786aa7--white-home-decor-home-design-white.jpg");
 
   function getImages(x) {
-   $('#image').attr('src', x).hide(0).fadeIn(500);
-  }
+   $('#image').attr('src', x).hide().fadeIn(700);
+   $('#image').attr('src', x).hide().fadeIn(500);
+    }
   var image1 = "https://s-media-cache-ak0.pinimg.com/736x/44/e7/e3/44e7e3896348dfc49731d5f61ac5344c--eat-in-kitchen-kitchen-nook.jpg";
   var image2 = "https://images.unsplash.com/photo-1498842812179-c81beecf902c?dpr=2&auto=format&fit=crop&w=1500&h=2250&q=80&cs=tinysrgb&crop=";
   var image3 = "https://images.unsplash.com/photo-1476891626313-2cecb3820a69?dpr=2&auto=format&fit=crop&w=1500&h=2437&q=80&cs=tinysrgb&crop=";
